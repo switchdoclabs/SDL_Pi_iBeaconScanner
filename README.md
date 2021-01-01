1756f47f36906e0fd574999e101da95fe3fc5229
SDL <you@example.com>
2021-01-01 12:48:30 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Python3 BlueTooth Device Scanner
+
+SwitchDoc Labs, LLC
+December 2020
+
+blescanner is a python program designed to read iBeacon advertizments using a Bluetooth Dongle on a Raspberry Pi
+
+To test, "sudo python3 testblescanner.py"
+
+For the Book from APress: "Raspberry Pi IoT Projects: Prototyping Experiments for Makers" 
+
+
---
+++ testblescan.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+# test BLE Scanning software
+# SwitchDoc Labs December 2020 
+
+import blescan
+import sys
+
+import bluetooth._bluetooth as bluez
+
+dev_id = 0
+try:
+	sock = bluez.hci_open_dev(dev_id)
+	print ("ble thread started")
+
+except:
+    print ("error accessing bluetooth device...")
+    sys.exit(1)
+
+blescan.hci_le_set_scan_parameters(sock)
+blescan.hci_enable_le_scan(sock)
+
+while True:
+	returnedList = blescan.parse_events(sock, 10)
+	print ("----------")
+	for beacon in returnedList:
+		print (beacon)
+
